d6c919f3e9fe56be477215b875275795bbc80614
Nibir <mz.nibir@gmail.com>
2021-06-25 17:07:56 +0600

    je login obsthay ase sei author er vetor jabe (use form_valid and uuid to update slug field)

--- My_Blog_Project/App_Blog/views.py
+++ My_Blog_Project/App_Blog/views.py
@@ -4,6 +4,8 @@ from App_Blog.models import Blog, Comment, Like
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+# unique id generate kore
+import uuid
 
 # Create your views here.
 
@@ -13,4 +15,14 @@ def blog_list(request):
 class CreateBlog(LoginRequiredMixin, CreateView):
     model = Blog
     template_name = 'App_Blog/create_blog.html'
-    fields = '__all__'
+    # je je fields rakhte cai
+    fields = ('blog_title', 'blog_content', 'blog_image')
+
+    def form_valid(self, form):
+        blog_obj = form.save(commit=False)
+        blog_obj.author = self.request.user
+        title = blog_obj.blog_title
+        # space replace by hipane...tar sathe extra enique id jora dibe
+        blog_obj.slug = title.replace(" ", "-") + "-" + str(uuid.uuid4())
+        blog_obj.save()
+        return HttpResponseRedirect(reverse('index'))
