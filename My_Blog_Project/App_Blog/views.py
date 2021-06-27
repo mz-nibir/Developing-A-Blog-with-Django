3d9dc7a0babe57ddcbef55f8a595403c8670500f
Nibir <mz.nibir@gmail.com>
2021-06-27 23:57:59 +0600

    Like added

--- My_Blog_Project/App_Blog/views.py
+++ My_Blog_Project/App_Blog/views.py
@@ -37,6 +37,13 @@ def blog_details(request, slug):
     blog = Blog.objects.get(slug=slug)
     # generating the comment form
     comment_form = CommentForm()
+
+    already_liked = Like.objects.filter(blog=blog, user=request.user)
+    # jodi kono obj theke thake...
+    if already_liked:
+        liked = True
+    else:
+        liked = False
     # Jodi form ti sumit kora hoy
     if request.method == 'POST':
         # form er vetor sumit kora info ashbe
@@ -53,4 +60,30 @@ def blog_details(request, slug):
 
 
 
-    return render(request, 'App_Blog/blog_details.html', context={'blog':blog, 'comment_form':comment_form})
+    return render(request, 'App_Blog/blog_details.html', context={'blog':blog, 'comment_form':comment_form, 'liked':liked})
+
+@login_required
+def liked(request, pk):
+    # je blog er pk pass koresi sei blog k anbe..
+    blog = Blog.objects.get(pk=pk)
+    user = request.user
+    # alreay blog ta k ei user like koreche ki na...like er modde filter korbe jei blog ki ase tar kono object ase ki na
+    already_liked = Like.objects.filter(blog=blog, user=user)
+    # jodi like na hoy(kono obj e jodi khuje na pay)
+    if not already_liked:
+        # like model a notun ekti obj create korsi jekhane..(blog=blog, user=user)
+        liked_post = Like(blog=blog, user=user)
+        liked_post.save()
+    return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug':blog.slug}))
+
+
+@login_required
+def unliked(request, pk):
+    # je blog er pk pass koresi sei blog k anbe..
+        blog = Blog.objects.get(pk=pk)
+        user = request.user
+        # alreay blog ta k ei user like koreche ki na...like er modde filter korbe jei blog ki ase tar kono object ase ki na
+        already_liked = Like.objects.filter(blog=blog, user=user)
+        # jodi like thake tahole delete kore dibe...
+        already_liked.delete()
+        return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug':blog.slug}))
