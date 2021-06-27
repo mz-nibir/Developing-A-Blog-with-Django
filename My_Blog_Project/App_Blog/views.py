7f5d6be15ab918f90415b66edd39c94b0da05322
Nibir <mz.nibir@gmail.com>
2021-06-27 17:02:08 +0600

    comment write

--- My_Blog_Project/App_Blog/views.py
+++ My_Blog_Project/App_Blog/views.py
@@ -4,6 +4,7 @@ from App_Blog.models import Blog, Comment, Like
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from App_Blog.forms import CommentForm
 # unique id generate kore
 import uuid
 
@@ -34,5 +35,22 @@ class CreateBlog(LoginRequiredMixin, CreateView):
 @login_required
 def blog_details(request, slug):
     blog = Blog.objects.get(slug=slug)
+    # generating the comment form
+    comment_form = CommentForm()
+    # Jodi form ti sumit kora hoy
+    if request.method == 'POST':
+        # form er vetor sumit kora info ashbe
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            # commont form er  user hobe ..akhon login kora user
+            comment.user = request.user
+            # kon blog er sathe connect hobe (ei blog page ai)
+            comment.blog = blog
+            comment.save()
+            # je blog page a asi sei page ei niye ashte hobe....jehetu eti argument accecpt kore tai arg pass korte hobe slug er moddhe
+            return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug':slug}))
 
-    return render(request, 'App_Blog/blog_details.html', context={'blog':blog})
+
+
+    return render(request, 'App_Blog/blog_details.html', context={'blog':blog, 'comment_form':comment_form})
